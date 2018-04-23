3676f5f6fd6a3dc5c2e56bed92cb40a20ad703a9
Jeff Weyer <jweyer@snap-software.com>
2018-04-22 22:23:37 -0700

    Additional tweaks for typescript, maps and coverage.

--- karma.conf.js
+++ karma.conf.js
@@ -28,26 +28,28 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.ts': ['typescript'],
-      '**/*.html': ['html2js'],
-      '**/*.json': ['json_fixtures'],
-      '**/*.js': ['coverage']
+      'src/**/*.ts': ['typescript', 'coverage'],
+      'src/**/*.js': ['coverage'],
+      'spec/**/*.ts': ['typescript'],
+      'spec/**/*.html': ['html2js'],
+      'spec/**/*.json': ['json_fixtures']
     },
 
-    // regarding json preprocessing
     jsonFixturesPreprocessor: {
       variableName: '__json__'
     },
 
     typescriptPreprocessor: {
       options: {
-        sourceMap: true, // (optional) Generates corresponding .map file.
-        outDir: 'dist/',
-        target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
-        module: 'commonjs', // (optional) Specify module code generation: 'commonjs' or 'amd'
-        noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type.
-        noResolve: false, // (optional) Skip resolution and preprocessing.
-        removeComments: true // (optional) Do not emit comments to output.
+        outDir: 'dist',
+        inlineSourceMap: true,
+        sourceMap: false, // (optional) Generates corresponding .map file. 
+        target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5' 
+        module: 'commonjs', // (optional) Specify module code generation: 'commonjs' or 'amd' 
+        noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type. 
+        noResolve: false, // (optional) Skip resolution and preprocessing. 
+        removeComments: true, // (optional) Do not emit comments to output. 
+        concatenateOutput: false // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false. 
       },
       transformPath: function (path) {
         return path.replace(/\.ts$/, '.js');
@@ -55,16 +57,18 @@ module.exports = function (config) {
     },
 
     // test results reporter to use
-    // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'coverage'],
 
 
     coverageReporter: {
+      type: 'html',
+      dir: 'coverage',
       instrumenterOptions: {
         istanbul: { noCompact: true }
       }
     },
+
 
     // web server port
     port: 9876,
